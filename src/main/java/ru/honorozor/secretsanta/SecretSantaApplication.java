acb45c5b645b87eb3d3dbc6ce26ca07d2fe15a41
saymyname <oakorotkov@alfabank.ru>
2021-02-09 17:30:46 +0300

    renamed main class

--- src/main/java/ru/honorozor/secretsanta/SecretSantaApplication.java
+++ src/main/java/ru/honorozor/secretsanta/SecretSantaApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 @SpringBootApplication
-public class SecretsantaApplication {
+public class SecretSantaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecretsantaApplication.class, args);
+        SpringApplication.run(SecretSantaApplication.class, args);
     }
 
 }
