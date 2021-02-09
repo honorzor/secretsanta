6097a571491e677343f0e8c0ea7c0be4dea0d061
ivantrapeznikov <ITrapeznikov@alfabank.ru>
2021-02-09 17:07:39 +0300

    refactor setting

--- src/main/java/ru/honorozor/secretsanta/config/MailConfig.java
+++ src/main/java/ru/honorozor/secretsanta/config/MailConfig.java
@@ -3,24 +3,35 @@ package ru.honorozor.secretsanta.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import ru.honorozor.secretsanta.settings.EmailSetting;
 
 import java.util.Properties;
 
 @Configuration
 public class MailConfig {
 
+    private final EmailSetting emailSetting;
+
+    public MailConfig(EmailSetting emailSetting) {
+        this.emailSetting = emailSetting;
+    }
+
     @Bean
     public JavaMailSenderImpl getJavaMailSenderImpl (){
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost("smtp.gmail.com");
-        javaMailSender.setPort(587);
-        javaMailSender.setProtocol("smtp");
-        javaMailSender.setUsername("mailbox");
-        javaMailSender.setPassword("password");
-
-        Properties javaMailProperties = javaMailSender.getJavaMailProperties();
-        javaMailProperties.setProperty("mail.smtp.auth","true");
-        javaMailProperties.setProperty("mail.smtp.starttls.enable","true");
+        javaMailSender.setHost(emailSetting.getHost());
+        javaMailSender.setPort(emailSetting.getPort());
+        javaMailSender.setProtocol(emailSetting.getProtocol());
+        javaMailSender.setUsername(emailSetting.getUsername());
+        javaMailSender.setPassword(emailSetting.getPassword());
+        javaMailSender.setJavaMailProperties(getProps());
         return javaMailSender;
+    }
+
+    private Properties getProps() {
+        Properties mailProp = new Properties();
+        mailProp.setProperty("mail.smtp.auth",emailSetting.getAuth());
+        mailProp.setProperty("mail.smtp.starttls.enable",emailSetting.getStarttlsEnable());
+        return mailProp;
     }
 }
