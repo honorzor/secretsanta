4651a8de05812334c99b51ed7a9af3f91808c425
ivantrapeznikov <ITrapeznikov@alfabank.ru>
2021-02-12 12:51:47 +0300

    added mapstruct for user , refactor test convertor

--- backend/src/main/java/ru/honorozor/secretsanta/mapper/UserMapper.java
+++ backend/src/main/java/ru/honorozor/secretsanta/mapper/UserMapper.java
@@ -1,8 +1,6 @@
 package ru.honorozor.secretsanta.mapper;
 
-import org.mapstruct.IterableMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValueMappingStrategy;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 import ru.honorozor.secretsanta.dto.UserDTO;
 import ru.honorozor.secretsanta.model.User;
@@ -13,6 +11,14 @@ import java.util.List;
 public interface UserMapper {
 
     UserMapper mapper = Mappers.getMapper(UserMapper.class);
+
+    @Mappings({
+            @Mapping(source = "email", target = "email"),
+            @Mapping(source = "name", target = "name"),
+            @Mapping(source = "filter", target = "filter"),
+            @Mapping(source = "toBuyGift", target = "emailToBuyGift")
+    })
+    User userDtoToEntity(UserDTO userDto);
 
     @IterableMapping(nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
     List<User> usersDtoToEntities(List<UserDTO> usersInGame);
